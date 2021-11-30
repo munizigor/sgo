70c301cb8e27ba924fa87d773a1b765ecbf4a42c
Igor Muniz da Silva <igor.silva@cocb.ssp.df.gov.br>
2021-11-30 16:02:59 -0300

    Addon QTOs externas pronto

--- js/alerta_externo.js
+++ js/alerta_externo.js
@@ -1,63 +1,37 @@
 function alertar_compartilhamento () {
-    //criando tag style para glowing buttons
-    document.head.insertAdjacentHTML("beforeend", `
-    <style>
-        @keyframes blinkingBackground{
-            0%		{ background-color: #10c018;}
-            25%		{ background-color: #1056c0;}
-            50%		{ background-color: #ef0a1a;}
-            75%		{ background-color: #254878;}
-            100%	        { background-color: #04a1d5;}
-        }
-    </style>
-    `)
 
     //iterando pelas linhas da tabela
     tabela = document.getElementById("dt_basic").getElementsByTagName("tbody")[0];
     tab_len = tabela.rows.length-1
 	for (var i = tab_len; row = tabela.rows[i]; i--) {
-		recurso = row.getElementsByTagName("td");
-        link = recurso[0].getElementsByTagName("a")[0]
-        link_txt = link.innerText
+		let recurso = row.getElementsByTagName("td");
+        let link = recurso[0].getElementsByTagName("a")[0]
+        let link_txt = link.innerText
         if (link_txt.substring(8,11)!="002") {
-            ocorrencia_url = location.origin+"/atendimento/imprimirdetalhamentocompleto?cod_teleatendimento="+recurso[0].id.toString()
-            console.log(ocorrencia_url)
-            $.ajax({
-                type: "GET",
-                url: ocorrencia_url,
-                dataType: "html",
-            success: function (data) {
+            let cod_recurso = recurso[0].id.toString()
+            $.get(location.origin+"/atendimento/imprimirdetalhamentocompleto?cod_teleatendimento="+cod_recurso).then(function(data){
                 var el = document.createElement('html');
                 el.innerHTML = data
                 divs = el.getElementsByClassName("panel-title")
-                for (i=0;atendimentos = divs[i];i++) {
-                    console.log(atendimentos.innerText+" ----- "+recurso[0].id)
-                    if (atendimentos.innerText.includes("CBMDF")) {
-                        // console.log("Ocorrencia "+recurso[0].id+" atendida pelo CBMDF")
+                divs_list = ""
+                for (j=0;atendimentos = divs[j];j++) {
+                    divs_list+= atendimentos.innerText
                     }
-                    else {
-                        // console.log("Ocorrencia "+recurso[0].id+" não atendida pelo CBMDF")
-                        // recurso[5].innerHTML="<h3><strong>QTO EXTERNA<br><br>VERIFICAR SE HOUVE DESPACHO</strong></h3>"
-                        // row.style.backgroundColor="indianred";
-                        // tabela.insertAdjacentElement('afterbegin',row)
-                    }
+                return divs_list
+            }).then( function(){
+                if (!divs_list.includes("CBMDF")) {
+                        recurso[5].innerHTML="<h3><strong>QTO SEM DESPACHO<br><br>DESPACHAR IMEDIATAMENTE</strong></h3>"
+                        console.log(recurso)
+                        recurso[0].parentNode.style.backgroundColor="indianred";
+                        tabela.insertAdjacentElement('afterbegin',recurso[0].parentNode)
+                }
+            }
+                )
 
                 }
-                
-
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                console.log(XMLHttpRequest);
-                console.log(textStatus);
-                console.log(errorThrown);
-            }
-            })
-
-
         }
       }
-}
+
 //TODO: COlocar alerta sonoro
-//TODO: Vermelho menos intenso no background
 alertar_compartilhamento ();
 
